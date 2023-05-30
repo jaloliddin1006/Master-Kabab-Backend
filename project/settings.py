2806fae3e94e2207147630b915affb010c13fbf7
Behzod <behzodtuit@gmail.com>
2023-05-30 12:32:56 +0500

    deploy

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-u1pq!exxs8b28di#7dz47pg)t^c-%qumns3g7l6cu@*qot--@f"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','159.223.18.25','masterkabab.uz','www.masterkabab.uz']
 # Application definition
@@ -157,22 +157,22 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 ############### Django Jazzmin #######
 JAZZMIN_SETTINGS = {
     # title of the window (Will default to current_admin_site.site_title if absent or None)
-    "site_title": "Little italy Admin",
+    "site_title": "Master Kabab Admin",
 
     # Title on the login screen (19 chars max) (defaults to current_admin_site.site_header if absent or None)
-    "site_header": "Little Italy",
+    "site_header": "Master Kabab",
 
     # Title on the brand (19 chars max) (defaults to current_admin_site.site_header if absent or None)
-    "site_brand": "Little Italy",
+    "site_brand": "Master Kabab",
 
     # Logo to use for your site, must be present in static files, used for brand on top left
-    "site_logo":None,
+    "site_logo":"images/logo.png",
 
     # Logo to use for your site, must be present in static files, used for login form logo (defaults to site_logo)
-    "login_logo": None,
+    "login_logo": "images/logo.png",
 
     # Logo to use for login form in dark themes (defaults to login_logo)
-    "login_logo_dark": None,
+    "login_logo_dark": "images/logo.png",
 
     # CSS classes that are applied to the logo above
     "site_logo_classes": None,
@@ -181,17 +181,17 @@ JAZZMIN_SETTINGS = {
     "site_icon": None,
 
     # Welcome text on the login screen
-    "welcome_sign": "Welcome to Little Italy",
+    "welcome_sign": "Welcome to Master Kabab",
 
     # Copyright on the footer
-    "copyright": "Little Italy Ltd",
+    "copyright": "Master Kabab Ltd",
 
     # List of model admins to search from the search bar, search bar omitted if excluded
     # If you want to use a single search field you dont need to use a list, you can use a simple string 
     "search_model": ['myapp.BotUser','myapp.Category','myapp.Product'],
 
     # Field name on user model that contains avatar ImageField/URLField/Charfield or a callable that receives the user
-    "user_avatar": None,
+    "user_avatar": "images/logo.png",
 
     ############
     # Top Menu #
